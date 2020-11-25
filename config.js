95a3214aa057563f2d9353d9f5585122c3da6cf8
JaimyVanGyseghem <jaimy.vangyseghem@gmail.com>
2020-11-25 10:56:14 +0100

    safety

--- config.js
+++ config.js
@@ -4,4 +4,5 @@ dotenv.config();
 module.exports = {
     port: process.env.PORT,
     port2: process.env.PORT2,
+    link: process.env.LINK,
 }
--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const path = require("path");
 const app = express();
 
 const mongoose = require("mongoose");
-const { port, port2 } = require("./config");
 
-mongoose.connect("mongodb+srv://webpgm4:webpgm4@cluster0.pesk7.mongodb.net/cluster0?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+const { port, port2, link } = require("./config");
+
+mongoose.connect(link, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("mongodb is connected successfully!");
 });
 
